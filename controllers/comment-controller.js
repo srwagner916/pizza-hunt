7cb2e880694ea2ec35203af4408a0f52aa6316bc
Shay Wagner <srwagner916@gmail.com>
2021-06-21 17:45:41 -0500

    crud functionality for comments.  comment model.  display pizzas on homepage

---
+++ controllers/comment-controller.js
@@ -0,0 +1,47 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+  // add comment
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ msg: 'No pizza found with this ID!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then(deletedComment => {
+        if (!deletedComment) {
+          return res.status(404).json({ msg: 'No comment with this ID!' });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ msg: 'No pizza found with this ID!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  }
+};
+
+module.exports = commentController;
